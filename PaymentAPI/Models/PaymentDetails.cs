c9520e834c8c93a967d4f708e780e35311a703b5
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-08-27 19:10:23 +0200

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column (TypeName ="navarchar (100)")]
+        public string CardOwnerName { get; set; }
+        [Column(TypeName = "navarchar (16)")]
+
+        public string CardNumber { get; set; }
+        [Column(TypeName = "navarchar (5)")]
+
+        public string ExpritionDate { get; set; }
+        [Column(TypeName = "navarchar (3)")]
+        public string SecurityCode { get; set; }
+
+
+    }
+}
