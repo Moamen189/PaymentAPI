170b28aa530da1ea40a70de184c4788a8702c1c5
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-08-28 17:31:30 +0200

    ClientStrucute

--- PaymentClient/PaymentApp/src/app/payment-details/payment-details-form/payment-details-form.component.ts
+++ PaymentClient/PaymentApp/src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+// import { Component, OnInit } from '@angular/core';
+// import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 
-@Component({
-  selector: 'app-payment-details-form',
-  templateUrl: './payment-details-form.component.html',
-  styles: [
-  ]
-})
-export class PaymentDetailsFormComponent implements OnInit {
+// @Component({
+//   selector: 'app-payment-details-form',
+//   templateUrl: './payment-details-form.component.html',
+//   styles: [
+//   ]
+// })
+// export class PaymentDetailsFormComponent implements OnInit {
 
-  constructor(public service:PaymentDetailService) { }
+//   constructor(public service:PaymentDetailService) { }
 
-  ngOnInit(): void {
-  }
+//   ngOnInit(): void {
+//   }
 
-}
+// }
